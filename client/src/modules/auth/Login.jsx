6fc0b250a3430bb85d31baeee5f3e3b2f2ea9242
Ahmed <sss.ahm7@gmail.com>
2023-09-19 19:21:33 +0500

    updated login page

--- client/src/modules/auth/Login.jsx
+++ client/src/modules/auth/Login.jsx
@@ -1,45 +1,88 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import AuthLayout from '../../layout/AuthLayout';
 import { Link } from 'react-router-dom';
 import '../../assets/css/Login.css';
 
 const Login = () => {
 
+  const [credential, setCredential] = useState({
+    email: "",
+    password: "",
+  });
+
+  const login = (e) => {
+    e.preventDefault();
+    // signIn({ ...credential })
+    //   .then((response) => {
+    //     const authUser = response.data.user;
+
+    //     let isAuthCookiesSet = setAuthCookie({
+    //       token: response.data.auth_token,
+    //       expiresIn: response.data.exp,
+    //       tokenType: "Bearer",
+    //       authState: {},
+    //     });
+
+    //     if (isAuthCookiesSet) {
+    //       navigate("/dashboard");
+    //     } else {
+    //       toastrOnTopCenter("Something went wrong, try later", "error");
+    //     }
+    //   })
+    //   .catch((errors) => {
+    //     setErrorBag(errors);
+    //   })
+    //   .finally(() => {
+    //     setProcessing(false);
+    //   });
+  }
+
   return (
-    <>
-      <div className="auth-card">
-        <div className="card">
-          <div className="login">
-            <h2 className='text-center mb-4'>Login</h2>
-            <p className='text-center mb-4'>Hey, Enter your details to get sign in to your account.</p>
-          </div>
-          <div className="login-inputs">
-            <form>
-              <div className="form-row">
-                <div className="col-md-12 mb-3">
-                  <input type="email" className="form-control" id="email" placeholder="Enter Email" required />
-                  <div className="invalid-feedback">
-                    Enter Email
+    <div className="row py-5">
+      <div className="col-md-12 my-auto">
+        <div className="auth-card">
+          <div className="card">
+            <div className="login">
+              <h2 className='text-center mb-4'>Login</h2>
+              <p className='text-center mb-4'>Hey, Enter your details to get sign in to your account.</p>
+            </div>
+            <div className="login-inputs">
+              <form onSubmit={login}>
+                <div className="form-row">
+                  <div className="col-md-12 mb-3">
+                    <input
+                      className="form-control"
+                      type="email"
+                      value={credential.email}
+                      onChange={(e) => setCredential({ ...credential, email: e.target.value })}
+                      placeholder="Enter Email"
+                      required />
+                  </div>
+                  <div className="col-md-12 mb-3">
+                    <input
+                      type="password"
+                      className="form-control"
+                      value={credential.password}
+                      onChange={(e) => setCredential({ ...credential, password: e.target.value })}
+                      placeholder="Enter Password"
+                      required />
+                    <div className="invalid-feedback">
+                      Enter Password
+                    </div>
                   </div>
                 </div>
                 <div className="col-md-12 mb-3">
-                  <input type="password" className="form-control" id="password" placeholder="Enter Password" required />
-                  <div className="invalid-feedback">
-                    Enter Password
-                  </div>
+                  <button className="btn btn-primary w-100" type="submit">Sign in</button>
                 </div>
+              </form>
+              <div className='text-center'>
+                <p>Don't have an account? <Link to="/register" className='fw-bold'>Sign up</Link></p>
               </div>
-              <div className="col-md-12 mb-3">
-                <button className="btn btn-primary w-100" type="submit">Sign in</button>
-              </div>
-            </form>
-            <div className='text-center'>
-              <p>Don't have an account? <Link to="/register" className='fw-bold'>Sign up</Link></p>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- client/src/modules/auth/Register.jsx
+++ client/src/modules/auth/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AuthLayout from '../../layout/AuthLayout';
 import '../../assets/css/Register.css';
@@ -6,56 +6,73 @@ import '../../assets/css/Register.css';
 const Register = () => {
 
   return (
-    <>
-      <div className="auth-card">
-        <div className="card">
-          <div className="register">
-            <h2 className='text-center mb-4'>Register</h2>
-            <p className='text-center mb-4'>Hey, Enter your details to get signed up to your account.</p>
+    <div className="row">
+      <div className="col-md-12 py-5">
+        <div className="auth-card-register">
+          <div className="card">
+            <div className="register">
+              <h2 className='text-center mb-4'>Register</h2>
+              <p className='text-center mb-4'>Hey, Enter your details to get signed up to your account.</p>
+            </div >
+            <div className="register-inputs">
+              <form onSubmit={() => { }}>
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Full Name"
+                      required
+                    />
+                  </div>
+                  <div className="col-md-6 mb-3">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                      required
+                    />
+                  </div>
+
+                  <div className="col-md-12 mb-3">
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="Phone Number"
+                      required
+                    />
+                  </div>
+                  <div className="col-md-12 mb-3">
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Password"
+                      required
+                    />
+                  </div>
+                  <div className="col-md-12 mb-3">
+                    <input
+                      type="password"
+                      className="form-control"
+                      required
+                      placeholder="Confirm Password"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12 mb-3">
+                  <button className="btn btn-primary w-100" type='submit'>
+                    Sign up
+                  </button>
+                </div>
+                <div className='text-center'>
+                  <p>Already have an account? <Link to="/login" className='fw-bold'>Sign in</Link></p>
+                </div>
+              </form>
+            </div>
           </div >
-          <div className="register-inputs">
-            <form>
-              <div className="row">
-                <div className="col-md-12 mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    autoComplete="off"
-                    placeholder="Username"
-                    required />
-                </div>
-                <div className="col-md-12 mb-3">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    required
-                    placeholder="Password"
-                  />
-                </div>
-                <div className="col-md-12 mb-3">
-                  <input
-                    type="password"
-                    className="form-control"
-                    required
-                    placeholder="Confirm Password"
-                  />
-                </div>
-              </div>
-              <div className="col-md-12 mb-3">
-                <button className="btn btn-primary w-100">
-                  Sign up
-                </button>
-              </div>
-              <div className='text-center'>
-                <p>Already have an account? <Link to="/login" className='fw-bold'>Sign in</Link></p>
-              </div>
-            </form>
-          </div>
         </div >
-      </div >
-    </>
+      </div>
+    </div>
   )
 }
 
